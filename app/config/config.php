f44f52bbb81d5de6e3af189c07835feeafe7104b
santiago.gimenez <santiagogimenez@outlook.com.ar>
2019-02-28 17:07:30 -0300

    Patched

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$admEmail = 'santiagogimenez@outlook.com.ar';
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
@@ -379,7 +378,7 @@ $config['encryption_key'] = hex2bin('02e9889de5389531894a8c252b1d236a');
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir(); // EDITADO PARA FUNCIONAR EN HEROKU
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
