b8ac4ac17e523e10332f1f9d33747c98555dce20
Santiago Gimenez <42920751+Chiqui1234@users.noreply.github.com>
2019-04-05 16:07:57 -0300

    Updated!

--- js/getSubForums.js
+++ js/getSubForums.js
@@ -2,69 +2,55 @@ function changeSubforums() {
     // Cambia los subforos en función del foro elegido.
     // Es de edición manual... pero más rápido que abrir consultas a la BD
     // Y muy útil porque no vamos a crear foros "todos los días", es contenido -prácticamente- estático
-
+    console.log('Obtención de foros activada');
     var forumNameOption = document.getElementById('forum');
     var forumNameIndex = forumNameOption.selectedIndex;
     var selectedForum = forumNameOption.options[forumNameIndex].value;
-
     var subforumOption = document.getElementById('subforum');
 
-    var pivot = document.getElementById('pivot'); // Para la tercer opción, que no deberá aparecer siempre
+    if ( selectedForum === 'computacion' ) {
+        document.getElementById('C').style.display = 'none';
+        document.getElementById('Python').style.display = 'none';
+        document.getElementById('Web').style.display = 'none';
+        document.getElementById('Otros').style.display = 'none';
 
-    if( selectedForum === 'programacion' || selectedForum === 'computacion'
-    || selectedForum === 'disenografico' || selectedForum === 'audio'
-    || selectedForum === 'video' ) {
-        // Los subforos son: "Aportes" y "Preguntas"
-        subforumOption.options[0].text = 'Aportes';
-        subforumOption.options[1].text = 'Preguntas';
-        subforumOption.options[0].value = 'aportes';
-        subforumOption.options[1].value = 'preguntas';
+        document.getElementById('2D').style.display = 'none';
+        document.getElementById('3D').style.display = 'none';
+        document.getElementById('Animacion').style.display = 'none';
+        document.getElementById('Otros').style.display = 'none';
+
+        document.getElementById('Ideas').style.display = 'none';
+        document.getElementById('Trabajo').style.display = 'none';
+        document.getElementById('Otros').style.display = 'none';
+
+        document.getElementById('UTN').style.display = 'none';
+        document.getElementById('UBA').style.display = 'none';
+        document.getElementById('UADE').style.display = 'none';
+        document.getElementById('Otras').style.display = 'none';
+
+        document.getElementById('Staff').style.display = 'none';
+        document.getElementById('Economia').style.display = 'none';
+        document.getElementById('Proyectos').style.display = 'none';
+        document.getElementById('Ayuda').style.display = 'none';
+    }
+
+    if( selectedForum === 'programacion' ) {
         
-        pivot.style.display = "none"; // El pivot desaparece porque no hay tercer opción
+    }
+
+    if( selectedForum === 'disenografico' ) {
+        
     }
 
     if( selectedForum === 'emprenderismo' ) {
-        // Los subforos son: "Ideas", "Preguntas" y "Otras"
-        subforumOption.options[0].text = 'Ideas';
-        subforumOption.options[1].text = 'Preguntas';
-        subforumOption.options[0].value = 'ideas';
-        subforumOption.options[1].value = 'preguntas';
         
-        pivot.style.display = "block"; // Ahora aparece, porque este foro tiene tercer opción
-        pivot.value = 'otras';
-        pivot.text = 'Otras';
     }
 
     if( selectedForum === 'universidades' ) { 
-        // Los subforos son: "UBA", "UTN", "Otras"
-        subforumOption.options[0].text = 'UBA';
-        subforumOption.options[1].text = 'UTN';
-        subforumOption.options[0].value = 'uba';
-        subforumOption.options[1].value = 'preguntas';
-
-        pivot.style.display = "block"; // Ahora aparece, porque este foro tiene tercer opción
-        pivot.value = 'otras';
-        pivot.text = 'Otras';
-    }
-    
-    if( selectedForum === 'retro') {
-        subforumOption.options[0].text = 'PCs';
-        subforumOption.options[1].text = 'Consolas';
-        subforumOption.options[0].value = 'PCs';
-        subforumOption.options[1].value = 'Consolas';
-
-        pivot.style.display = "block"; // Ahora aparece, porque este foro tiene tercer opción
-        pivot.value = 'Otros';
-        pivot.text = 'Otros';
+        
     }
 
-    if( selectedForum === 'off-topic' ) {
-        // Los subforos son: "Economía/Política" y "Videojuegos"
-        subforumOption.options[0].text = 'Economía/Política';
-        subforumOption.options[1].text = 'Videojuegos';
-        subforumOption.options[0].value = 'economia-politica';
-        subforumOption.options[1].value = 'videojuegos';
-
-        pivot.style.display = "none"; // El pivot desaparece porque no hay tercer opción
+    if( selectedForum === 'off_topic' ) {
+        
     }
 }
